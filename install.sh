6c7696f2f60231baf9a599fb174a0508a77a32ea
Stas Kelvich <stas.kelvich@gmail.com>
2015-09-07 16:47:22 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+./install/bin/pg_ctl -D ./install/data1 stop
+./install/bin/pg_ctl -D ./install/data2 stop
+
+rm -rf install
+
+make install
+
+cd contrib/pg_xtm/
+
+make clean
+make
+make install
+
+cd ../..
+
+./install/bin/initdb -D ./install/data1
+./install/bin/initdb -D ./install/data2
+
+sed -i '' 's/#port =.*/port = 5433/' ./install/data2/postgresql.conf
+
+
+
+./install/bin/pg_ctl -D ./install/data1 -l ./install/data1/log start
+./install/bin/pg_ctl -D ./install/data2 -l ./install/data2/log start
+
+
+cd contrib/pg_dtm/dtmd
+make clean
+make
+./bin/dtmd
