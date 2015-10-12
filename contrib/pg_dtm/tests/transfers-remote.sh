a162e5a781dcd56c26110c9e30f116960a4c62dd
Stas Kelvich <stas.kelvich@gmail.com>
2015-10-12 19:19:58 +0300

    Farm deploy stuff

--- contrib/pg_dtm/tests/transfers-remote.sh
+++ contrib/pg_dtm/tests/transfers-remote.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 if [ ! -f ./transfers.linux ]; then
-    GOOS=linux GOARCH=386 go build -o ./transfers.linux ./transfers.go
+    GOOS=linux GOARCH=amd64 go build -o ./transfers ./transfers.go
 fi
 
-ansible master-workers -i farms/sai -m copy -a "src=transfers.linux dest=~/transfers"
-
-# ssh -p4208 br.theor.mephi.ru "./transfers -d 'dbname=postgres port=5432' -d 'dbname=postgres port=5433' -g -m"
-
-ansible single-worker -i farms/mephi -a "./transfers -d 'dbname=postgres port=5432' -d 'dbname=postgres port=5433' -g -m"
+ansible master-workers -i farms/mephi -m copy -a "src=transfers dest=~/transfers mode=a+x"
 
 
 
+ssh -p4207 br.theor.mephi.ru "./transfers -d 'dbname=postgres user=stas port=5432' -d 'dbname=postgres user=stas port=5433' -g -m"
 
-ssh s.kelvich@158.250.29.10 "./transfers -d 'host=158.250.29.10 dbname=postgres' -d 'host=158.250.29.9 dbname=postgres' -d 'host=158.250.29.8 dbname=postgres' -g -m -w 64"
+
+ssh s.kelvich@158.250.29.10 "./transfers -d 'host=158.250.29.10 dbname=postgres' -d 'host=158.250.29.9 dbname=postgres' -d 'host=158.250.29.8 dbname=postgres' -g -w 64"
+
+
+ssh -p4207 br.theor.mephi.ru "./transfers -d 'host=blade8 dbname=postgres user=stas port=5432' -d 'host=blade7 dbname=postgres user=stas port=5432' -d 'host=blade6 dbname=postgres user=stas port=5432' -d 'host=blade5 dbname=postgres user=stas port=5432' -d 'host=blade4 dbname=postgres user=stas port=5432' -d 'host=blade3 dbname=postgres user=stas port=5432' -g -m"
+
+
+
