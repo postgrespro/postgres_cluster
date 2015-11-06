b976bef03e5d5b89a0f0c479ca0003f06c9d9c9a
Stas Kelvich <stas.kelvich@gmail.com>
2015-11-06 15:19:43 +0300

    perf

--- tests/run_perf.sh
+++ tests/run_perf.sh
@@ -1,4 +1,11 @@
-go run *.go  \
+go run perf/*.go  \
 -C "dbname=postgres port=5432 sslmode=disable" \
 -C "dbname=postgres port=5433 sslmode=disable" \
--g -w 8 -r 1 -n 1000 -a 1000 -i
+-C "dbname=postgres port=5434 sslmode=disable" \
+-g -w 8 -r 1 -n 1000 -a 10000 -i
+
+go run perf/*.go  \
+-C "dbname=postgres port=5432 sslmode=disable" \
+-C "dbname=postgres port=5433 sslmode=disable" \
+-C "dbname=postgres port=5434 sslmode=disable" \
+-g -w 8 -r 1 -n 1000 -a 10000
