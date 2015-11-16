7317bb00ef19af8d979bc0ed33ea76500b9d45dc
Stas Kelvich <stas.kelvich@gmail.com>
2015-11-16 19:28:55 +0300

    delete username from run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,10 @@
 ./dtmbench  \
--c "dbname=postgres host=localhost user=knizhnik port=5432 sslmode=disable" \
--c "dbname=postgres host=localhost user=knizhnik port=5433 sslmode=disable" \
--c "dbname=postgres host=localhost user=knizhnik port=5434 sslmode=disable" \
--n 1000 -a 1000 -w 10 -r 1 $*
+-C "dbname=postgres host=localhost port=5432 sslmode=disable" \
+-C "dbname=postgres host=localhost port=5433 sslmode=disable" \
+-n 1000 -a 1010 -d 1000 -w 10 -r 1 -i $*
+
+
+./dtmbench  \
+-C "dbname=postgres host=localhost port=5432 sslmode=disable" \
+-C "dbname=postgres host=localhost port=5433 sslmode=disable" \
+-n 30000 -a 1010 -d 1000 -w 10 -r 1 $*
