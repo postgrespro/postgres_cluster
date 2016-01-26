d0509122c8cccd09bba256aaca45be0149bb80ca
Konstantin Knizhnik <knizhnik@garret.ru>
2016-01-26 17:49:23 +0300

    Update insbench

--- src/bin/insbench/run.sh
+++ src/bin/insbench/run.sh
@@ -2,5 +2,7 @@ echo Insert with 1 index
 ./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 0
 echo Insert with 9 indexex
 ./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 8
-echo Insert with 9 partial indexes
+echo Insert with 9 concurrently update partial indexes
 ./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 8 -u 1
+echo Insert with 9 frozen partial indexes
+./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 8 -u 10
