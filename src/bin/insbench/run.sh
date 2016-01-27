ced7bf450760f7fc51759b8c332cf09eacc5c5f2
Konstantin Knizhnik <knizhnik@garret.ru>
2016-01-27 20:16:32 +0300

    Use COPY in insbench

--- src/bin/insbench/run.sh
+++ src/bin/insbench/run.sh
@@ -1,8 +1,8 @@
 echo Insert with 1 index
-./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 0
+./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -q -C -x 0
 echo Insert with 9 indexex
-./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 8
+./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -q -C -x 8
 echo Insert with 9 concurrently update partial indexes
-./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 8 -u 1
+./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -q -C -x 8 -u 1
 echo Insert with 9 frozen partial indexes
-./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -x 8 -u 10
+./insbench -c "dbname=postgres host=localhost port=5432 sslmode=disable" -q -C -x 8 -u 100
