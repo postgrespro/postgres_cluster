ab927a6200220216a113d6c92a3ebd90fc025ddf
Stas Kelvich <stas.kelvich@gmail.com>
2016-02-24 19:45:28 +0300

    init script

---
+++ reinit.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+reinit_master() {
+	rm -rf install/data
+
+	./install/bin/initdb -A trust -D ./install/data
+
+	echo "max_prepared_transactions = 100" >> ./install/data/postgresql.conf
+	echo "shared_buffers = 512MB" >> ./install/data/postgresql.conf
+	echo "fsync = off" >> ./install/data/postgresql.conf
+	echo "log_checkpoints = on" >> ./install/data/postgresql.conf
+	echo "max_wal_size = 48MB" >> ./install/data/postgresql.conf
+	echo "min_wal_size = 32MB" >> ./install/data/postgresql.conf
+	echo "wal_level = logical" >> ./install/data/postgresql.conf
+	echo "wal_keep_segments = 64" >> ./install/data/postgresql.conf
+	echo "max_wal_senders = 2" >> ./install/data/postgresql.conf
+	echo "max_replication_slots = 2" >> ./install/data/postgresql.conf
+
+	echo '' > ./install/data/logfile
+
+	echo 'local replication stas trust' >> ./install/data/pg_hba.conf
+
+	./install/bin/pg_ctl -sw -D ./install/data -l ./install/data/logfile start
+	./install/bin/createdb stas
+	./install/bin/psql -c "create table t(id int);"
+}
+
+reinit_slave() {
+	rm -rf install/data_slave
+
+	./install/bin/pg_basebackup -D ./install/data_slave/ -R
+
+	echo "port = 5433" >> ./install/data_slave/postgresql.conf
+	echo "hot_standby = on" >> ./install/data_slave/postgresql.conf
+
+	echo '' > ./install/data_slave/logfile
+
+	./install/bin/pg_ctl -sw -D ./install/data_slave -l ./install/data_slave/logfile start
+}
+
+make install > /dev/null
+
+
+pkill -9 postgres
+reinit_master >> /dev/null
+
