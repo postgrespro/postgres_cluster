d4b6475f25e3681dcee3bc8b579263f8ecacbf49
Stas Kelvich <stas.kelvich@gmail.com>
2016-02-29 16:50:23 +0300

    new locks; false -> NULL

--- contrib/pglogical/pglogical_worker.c
+++ contrib/pglogical/pglogical_worker.c
@@ -407,7 +407,8 @@ pglogical_worker_shmem_startup(void)
 
 	if (!found)
 	{
-		PGLogicalCtx->lock = LWLockAssign();
+		// PGLogicalCtx->lock = LWLockAssign();
+		PGLogicalCtx->lock = &(GetNamedLWLockTranche("pglogical"))->lock;
 		PGLogicalCtx->supervisor = NULL;
 		PGLogicalCtx->total_workers = max_worker_processes;
 		memset(PGLogicalCtx->workers, 0,
@@ -431,7 +432,8 @@ pglogical_worker_shmem_init(void)
 	 * tries to allocate or free blocks from this array at once.  There won't
 	 * be enough contention to make anything fancier worth doing.
 	 */
-	RequestAddinLWLocks(1);
+	// RequestAddinLWLocks(1);
+	RequestNamedLWLockTranche("pglogical", 1);
 
 	/*
 	 * Whether this is a first startup or crash recovery, we'll be re-initing
