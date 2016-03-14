1a312b45119e4a91171ebfc5bc7426c9619cdd3e
Tom Lane <tgl@sss.pgh.pa.us>
2016-03-14 16:59:59 -0400

    Rethink representation of PathTargets.
    In commit 19a541143a09c067 I did not make PathTarget a subtype of Node,
    and embedded a RelOptInfo's reltarget directly into it rather than having
    a separately-allocated Node.  In hindsight that was misguided
    micro-optimization, enabled by the fact that at that point we didn't have
    any Paths with custom PathTargets.  Now that PathTarget processing has
    been fleshed out some more, it's easier to see that it's better to have
    PathTarget as an indepedent Node type, even if it does cost us one more
    palloc to create a RelOptInfo.  So change it while we still can.
    
    This commit just changes the representation, without doing anything more
    interesting than that.

--- contrib/postgres_fdw/postgres_fdw.c
+++ contrib/postgres_fdw/postgres_fdw.c
@@ -485,7 +485,7 @@ postgresGetForeignRelSize(PlannerInfo *root,
 	 * columns used in them.  Doesn't seem worth detecting that case though.)
 	 */
 	fpinfo->attrs_used = NULL;
-	pull_varattnos((Node *) baserel->reltarget.exprs, baserel->relid,
+	pull_varattnos((Node *) baserel->reltarget->exprs, baserel->relid,
 				   &fpinfo->attrs_used);
 	foreach(lc, fpinfo->local_conds)
 	{
@@ -536,7 +536,7 @@ postgresGetForeignRelSize(PlannerInfo *root,
 
 		/* Report estimated baserel size to planner. */
 		baserel->rows = fpinfo->rows;
-		baserel->reltarget.width = fpinfo->width;
+		baserel->reltarget->width = fpinfo->width;
 	}
 	else
 	{
@@ -553,7 +553,7 @@ postgresGetForeignRelSize(PlannerInfo *root,
 		{
 			baserel->pages = 10;
 			baserel->tuples =
-				(10 * BLCKSZ) / (baserel->reltarget.width +
+				(10 * BLCKSZ) / (baserel->reltarget->width +
 								 MAXALIGN(SizeofHeapTupleHeader));
 		}
 
@@ -2168,7 +2168,7 @@ estimate_path_cost_size(PlannerInfo *root,
 		 * between foreign relations.
 		 */
 		rows = foreignrel->rows;
-		width = foreignrel->reltarget.width;
+		width = foreignrel->reltarget->width;
 
 		/* Back into an estimate of the number of retrieved rows. */
 		retrieved_rows = clamp_row_est(rows / fpinfo->local_conds_sel);
@@ -3694,7 +3694,7 @@ postgresGetForeignJoinPaths(PlannerInfo *root,
 							&width, &startup_cost, &total_cost);
 	/* Now update this information in the joinrel */
 	joinrel->rows = rows;
-	joinrel->reltarget.width = width;
+	joinrel->reltarget->width = width;
 	fpinfo->rows = rows;
 	fpinfo->width = width;
 	fpinfo->startup_cost = startup_cost;
