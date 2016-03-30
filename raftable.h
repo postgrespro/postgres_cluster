98eb87d0d06d286911246ce5420db70ce7c2ef51
Konstantin Knizhnik <knizhnik@garret.ru>
2016-03-30 10:47:04 +0300

    Rename paxos to taftable and make last one optionally included

--- raftable.h
+++ raftable.h
@@ -1,11 +1,11 @@
-#ifndef __PAXOS_H__
-#define __PAXOS_H__
+#ifndef __RAFTABLE_H__
+#define __RAFTABLE_H__
 
-typedef struct PaxosTimestamp { 
+typedef struct RaftableTimestamp { 
 	time_t time;   /* local time at master */
 	uint32 master; /* master node for this operation */
-	uint32 psn;    /* PAXOS serial number */
-} PaxosTimestamp;
+	uint32 psn;    /* RAFTABLE serial number */
+} RaftableTimestamp;
 
 /*
  * Get key value.
@@ -15,18 +15,23 @@ typedef struct PaxosTimestamp {
  * If "ts" is not NULL, then it is assigned timestamp of last update of this value
  * If RAFT master is not accessible, then depending non value of "nowait" parameter, this funciton should either block until RAFT quorum is reached, either report error.
  */
-extern void* PaxosGet(char const* key, int* size, PaxosTimestamp* ts, bool nowait);
+extern void* RaftableGet(char const* key, int* size, RaftableTimestamp* ts, bool nowait);
 
 /*
  * Set new value for the specified key. IF value is NULL, then key should be deleted.
  * If RAFT master is not accessible, then depending non value of "nowait" parameter, this funciton should either block until RAFT quorum is reached, either report error.
  */
-extern void  PaxosSet(char const* key, void const* value, int size, bool nowait);
+extern void  RaftableSet(char const* key, void const* value, int size, bool nowait);
 
 /* 
  * If key doesn't exists or its value is not equal to the specified value then store this value and return true.
  * Otherwise do nothing and return false.
  * If RAFT master is not accessible, then depending non value of "nowait" parameter, this funciton should either block until RAFT quorum is reached, either report error.
  */
-extern bool  PaxosCAS(char const* key, char const* value, bool nowait);
+extern bool  RaftableCAS(char const* key, char const* value, bool nowait);
+
+typedef void* (*raftable_get_t)(char const* key, int* size, int timeout);
+typedef void (*raftable_set_t)(char const* key, void const* value, int size, int timeout);
+
+
 #endif
