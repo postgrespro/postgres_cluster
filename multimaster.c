aface56d1884e0ba57dff98ea8e3fe2778094b0a
Konstantin Knizhnik <knizhnik@garret.ru>
2016-03-31 09:04:54 +0300

    Undo changes around MtmRecoveryCaughtUp

--- multimaster.c
+++ multimaster.c
@@ -996,7 +996,6 @@ bool MtmRecoveryCaughtUp(int nodeId, XLogRecPtr slotLSN)
 	if (MtmIsRecoveredNode(nodeId)) { 
 		XLogRecPtr walLSN = GetXLogInsertRecPtr();
 		MtmLock(LW_EXCLUSIVE);
-#if 0
 		if (slotLSN == walLSN) {
 			if (BIT_CHECK(Mtm->nodeLockerMask, nodeId-1)) { 
 				elog(WARNING,"Node %d is caught-up", nodeId);	
@@ -1010,9 +1009,7 @@ bool MtmRecoveryCaughtUp(int nodeId, XLogRecPtr slotLSN)
 			BIT_CLEAR(Mtm->disabledNodeMask, nodeId-1);
 			Mtm->nNodes += 1;
 			caughtUp = true;
-		} else 
-#endif
-		if (!BIT_CHECK(Mtm->nodeLockerMask, nodeId-1)
+		} else if (!BIT_CHECK(Mtm->nodeLockerMask, nodeId-1)
 				   && slotLSN + MtmMinRecoveryLag > walLSN) 
 		{ 
 			/*
