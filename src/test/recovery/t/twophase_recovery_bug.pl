1a0268432108f9163fe6231e5bfba60c926ae952
Stas Kelvich <stas.kelvich@gmail.com>
2016-04-06 13:03:53 +0300

    fixes

--- src/test/recovery/t/twophase_recovery_bug.pl
+++ src/test/recovery/t/twophase_recovery_bug.pl
@@ -2,10 +2,10 @@ use strict;
 use warnings;
 use PostgresNode;
 use TestLib;
-use Test::More tests => 1;
+use Test::More tests => 2;
 
 # Setup master node
-my $node_master = get_new_node("Candie");
+my $node_master = get_new_node("master");
 $node_master->init(allows_streaming => 1);
 $node_master->append_conf('postgresql.conf', qq(
 max_prepared_transactions = 10
@@ -14,8 +14,8 @@ $node_master->start;
 $node_master->backup('master_backup');
 $node_master->psql('postgres', "create table t(id int)");
 
-# Setup master node
-my $node_slave = get_new_node('Django');
+# Setup slave node
+my $node_slave = get_new_node('slave');
 $node_slave->init_from_backup($node_master, 'master_backup', has_streaming => 1);
 $node_slave->start;
 
@@ -35,12 +35,11 @@ $node_master->psql('postgres', "
 	select * from t2 for share;
 	prepare transaction 'x';
 ");
-sleep 2;
+sleep 2; # wait for changes to arrive on slave
 $node_slave->teardown_node;
 $node_master->psql('postgres',"commit prepared 'x'");
 $node_slave->start;
 $node_slave->psql('postgres',"select count(*) from pg_prepared_xacts", stdout => \$psql_out);
-
 is($psql_out, '0', "Commit prepared on master while slave is down.");
-
-
+$node_slave->psql('postgres',"select sum(id) from t2", stdout => \$psql_out);
+is($psql_out, '2', "Check that tx changes are visible.");
