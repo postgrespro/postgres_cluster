b6d1e7154dc60c156a9843087fd720bc5e1bb10a
Stas Kelvich <stas.kelvich@gmail.com>
2016-04-20 14:53:34 +0300

    clean up run.pl

--- contrib/mmts/testeaux/run.pl
+++ contrib/mmts/testeaux/run.pl
@@ -5,10 +5,6 @@ use TestLib;
 use DBI;
 use File::Temp ();
 
-#use DBD::Pg ':async';
-
-#@{; eval { TestLib::end_av->object_2svref } || [] } = ();
-
 $File::Temp::KEEP_ALL = 1;
 
 my $cluster = new Cluster(3);
@@ -16,7 +12,4 @@ $cluster->init;
 $cluster->configure;
 $cluster->start;
 $cluster->detach;
-
-
-#sleep(3600);
 
