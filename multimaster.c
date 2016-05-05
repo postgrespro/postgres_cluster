b59ca54e29927876c6c6aed8b5af1a13c5dfaf43
Konstantin Knizhnik <knizhnik@garret.ru>
2016-05-05 18:11:55 +0300

    Increase node_disable_delay to 10 seconds

--- multimaster.c
+++ multimaster.c
@@ -1694,7 +1694,7 @@ _PG_init(void)
 		"Minamal amount of time (msec) between node status change",
 		"This delay is used to avoid false detection of node failure and to prevent blinking of node status node",
 		&MtmNodeDisableDelay,
-		1000,
+		10000,
 		1,
 		INT_MAX,
 		PGC_BACKEND,
