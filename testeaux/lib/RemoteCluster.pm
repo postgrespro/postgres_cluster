1e89f6a61e81ef07e367689b180426a3899fb8fa
Stas Kelvich <stas.kelvich@gmail.com>
2016-05-11 19:07:30 +0300

    test with splits

--- testeaux/lib/RemoteCluster.pm
+++ testeaux/lib/RemoteCluster.pm
@@ -80,14 +80,21 @@ sub configure
 			max_wal_senders = 10
 			wal_sender_timeout = 0
 			max_replication_slots = 10
+
+			tcp_keepalives_idle = 2
+			tcp_keepalives_interval = 1
+			tcp_keepalives_count = 2
+
 			shared_preload_libraries = 'raftable,multimaster'
 			multimaster.workers = 10
 			multimaster.queue_size = 10485760 # 10mb
 			multimaster.node_id = $id
 			multimaster.conn_strings = '$connstr'
-			multimaster.use_raftable = true
+			multimaster.use_raftable = false
 			multimaster.ignore_tables_without_pk = true
 			multimaster.twopc_min_timeout = 60000
+			multimaster.twopc_prepare_ratio = 1000 #%
+
 			raftable.id = $id
 			raftable.peers = '$raftpeers'
 		));
@@ -130,9 +137,9 @@ sub psql
 }
 
 # XXX: test
-my $cluster = new RemoteCluster('ssh-config');
-$cluster->init;
-$cluster->configure;
-$cluster->start;
-
+#my $cluster = new RemoteCluster('ssh-config');
+# $cluster->init;
+# $cluster->configure;
+# $cluster->start;
+# 
 1;
