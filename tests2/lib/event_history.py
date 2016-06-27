4b3a499766d9f07ead8acea1a6c9fb5d1620de19
Stas Kelvich <stas.kelvich@gmail.com>
2016-06-27 19:25:47 +0300

    better errors handling in bank_client

--- tests2/lib/event_history.py
+++ tests2/lib/event_history.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 import uuid
+import copy
 from multiprocessing import Queue
 
 
@@ -12,11 +13,10 @@ class EventHistory():
         self.running_events = {}
         self.last_aggregation = datetime.datetime.now()
         self.agg_template = {
-            'commit': 0,
-            'rollback': 0,
             'max_latency': 0.0,
             'running': 0,
-            'running_latency': 0.0
+            'running_latency': 0.0,
+            'finish': {}
         }
 
     def register_start(self, name):
@@ -43,18 +43,21 @@ class EventHistory():
                 self.running_events[event['event_id']] = event
             else:
                 # finish mark
-                if event['event_id'] in self.running_events:
-                    start_ev = self.running_events[event['event_id']]
-                    self.events.append({
-                        'name': start_ev['name'],
-                        'started_at': start_ev['time'],
-                        'finished_at': event['time'],
-                        'status': event['status']
-                    })
-                    self.running_events.pop(event['event_id'], None)
-                else:
+                if event['event_id'] not in self.running_events:
                     # found finish event without corresponding start
+                    print("ololololo!")
                     raise
+
+                start_ev = self.running_events[event['event_id']]
+                self.events.append({
+                    'name': start_ev['name'],
+                    'started_at': start_ev['time'],
+                    'finished_at': event['time'],
+                    'status': event['status']
+                })
+                self.running_events.pop(event['event_id'], None)
+
+        #print(self.events)
         return
 
     def aggregate(self):
@@ -63,32 +66,38 @@ class EventHistory():
         agg = {}
         for ev in self.events:
             if ev['finished_at'] < self.last_aggregation:
+                #print("cont")
                 continue
 
             if ev['name'] not in agg:
-                agg[ev['name']] = self.agg_template.copy()
+                agg[ev['name']] = copy.deepcopy(self.agg_template)
+                #print('-=-=-', agg)
 
             named_agg = agg[ev['name']]
             latency = (ev['finished_at'] - ev['started_at']).total_seconds()
-            named_agg[ev['status']] += 1
+
+            if ev['status'] not in named_agg['finish']:
+                named_agg['finish'][ev['status']] = 1
+            else:
+                named_agg['finish'][ev['status']] += 1
+
             if named_agg['max_latency'] < latency:
                 named_agg['max_latency'] = latency
 
         for value in self.running_events.itervalues():
+
             if value['name'] not in agg:
-                agg[value['name']] = self.agg_template.copy()
+                agg[value['name']] = copy.deepcopy(self.agg_template)
 
             named_agg = agg[value['name']]
             latency = (datetime.datetime.now() - value['time']).total_seconds()
-            if 'started' in named_agg:
-                named_agg['running'] += 1
-                if latency > named_agg['running_latency']:
-                    named_agg['running_latency'] = latency
-            else:
-                named_agg['running'] = 1
+
+            named_agg['running'] += 1
+            if named_agg['running_latency'] < latency:
                 named_agg['running_latency'] = latency
 
         self.last_aggregation = datetime.datetime.now()
+        #print("aggregeted!")
         return agg
 
     def aggregate_by(self, period):
