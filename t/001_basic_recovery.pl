7467c089aa803b6fd185342957088acbc246948d
Konstantin Knizhnik <knizhnik@garret.ru>
2016-08-08 13:01:08 +0300

    Slightly rewrite travis test

--- t/001_basic_recovery.pl
+++ t/001_basic_recovery.pl
@@ -43,17 +43,26 @@ $cluster->{nodes}->[2]->teardown_node;
 
 sleep(5); # Wait until failure of node will be detected
 
-my $ret = $cluster->psql(0, 'postgres', "insert into t values(4, 40);");
+diag("inserting 2 on node 1");
+my $ret = $cluster->psql(0, 'postgres', "insert into t values(2, 20);"); # this transaciton may fail
 diag "tx1 status = $ret";
 
-diag("inserting 2");
-my $ret = $cluster->psql(0, 'postgres', "insert into t values(2, 20);");
+diag("inserting 3 on node 2");
+my $ret = $cluster->psql(1, 'postgres', "insert into t values(3, 30);"); # this transaciton may fail
+diag "tx2 status = $ret";
+
+diag("inserting 4 on node 1 (can fail)");
+my $ret = $cluster->psql(0, 'postgres', "insert into t values(4, 40);"); 
+diag "tx1 status = $ret";
+
+diag("inserting 5 on node 2 (can fail)");
+my $ret = $cluster->psql(1, 'postgres', "insert into t values(5, 50);"); 
 diag "tx2 status = $ret";
 
 diag("selecting");
-$cluster->psql(1, 'postgres', "select v from t where k=2;", stdout => \$psql_out);
+$cluster->psql(1, 'postgres', "select v from t where k=4;", stdout => \$psql_out);
 diag("selected");
-is($psql_out, '20', "Check replication after node failure.");
+is($psql_out, '40', "Check replication after node failure.");
 
 ###############################################################################
 # Work after node start
@@ -62,25 +71,29 @@ is($psql_out, '20', "Check replication after node failure.");
 diag("starting node 2");
 $cluster->{nodes}->[2]->start;
 
-sleep(10); # Wait until recovery of node will be completed
-
 $cluster->psql(0, 'postgres', "select mtm.poll_node(3);");
+sleep(5); # Wait until recovery of node will be completed
 
-diag("inserting 3");
-$cluster->psql(0, 'postgres', "insert into t values(3, 30);");
-diag("inserting 4");
-$cluster->psql(1, 'postgres', "insert into t values(4, 40);");
+diag("inserting 6 on node 1 (can fail)");
+$cluster->psql(0, 'postgres', "insert into t values(6, 60);"); 
+diag("inserting 7 on node 2 (can fail)");
+$cluster->psql(1, 'postgres', "insert into t values(7, 70);");
 
-diag("selecting");
-$cluster->psql(2, 'postgres', "select v from t where k=3;", stdout => \$psql_out);
+diag("inserting 8 on node 1");
+$cluster->psql(0, 'postgres', "insert into t values(8, 80);");
+diag("inserting 9 on node 2");
+$cluster->psql(1, 'postgres', "insert into t values(9, 90);");
+
+diag("selecting from node 3");
+$cluster->psql(2, 'postgres', "select v from t where k=8;", stdout => \$psql_out);
 diag("selected");
 
-is($psql_out, '30', "Check replication after failed node recovery.");
+is($psql_out, '80', "Check replication after failed node recovery.");
 
-$cluster->psql(2, 'postgres', "select v from t where k=4;", stdout => \$psql_out);
+$cluster->psql(2, 'postgres', "select v from t where k=9;", stdout => \$psql_out);
 diag("selected");
 
-is($psql_out, '40', "Check replication after failed node recovery.");
+is($psql_out, '90', "Check replication after failed node recovery.");
 
 $cluster->stop();
 1;
