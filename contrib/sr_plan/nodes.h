c4860fb93801f4bffe6b72c931f50aa3b4f3c625
Dmitry Maslyuk <d.masluk@postgrespro.ru>
2016-08-30 14:46:08 +0300

    Add sr_plan as submodule

--- contrib/sr_plan/nodes.h
+++
@@ -1,8 +0,0 @@
-#define __attribute__(x)
-typedef int __int128;
-
-#include "pg_config.h"
-#include "postgres.h"
-#include "nodes/plannodes.h"
-#include "nodes/parsenodes.h"
-#include "nodes/value.h"
