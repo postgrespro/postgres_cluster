4aca16c6d77c838a84959ce8dcd49c38084c0330
Konstantin Knizhnik <knizhnik@garret.ru>
2016-10-05 19:44:37 +0300

    Check for in progress before setting HEAP_XMIN_INVALID hint

--- regress.sql
+++ regress.sql
@@ -1,4 +1,4 @@
-create extension multimaster;
+--create extension multimaster;
 ALTER DATABASE "regression" SET lc_messages TO 'C';
 ALTER DATABASE "regression" SET lc_monetary TO 'C';
 ALTER DATABASE "regression" SET lc_numeric TO 'C';
