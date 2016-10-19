e8cdd30f6f64f9de764bc6e6b8b0aed9e3fe125e
Stas Kelvich <stas.kelvich@gmail.com>
2016-10-19 19:44:17 +0300

    Change the way to build containers

--- Dockerfile
+++ Dockerfile
@@ -1,52 +1,19 @@
-# vim:set ft=dockerfile:
-FROM debian:jessie
+FROM alpine:latest
 
-# explicitly set user/group IDs
-RUN groupadd -r postgres --gid=999 && useradd -r -g postgres --uid=999 postgres
+RUN apk add --update gcc libc-dev bison flex readline-dev zlib-dev perl make
 
-# make the "en_US.UTF-8" locale so postgres will be utf-8 enabled by default
-RUN apt-get update && apt-get install -y locales && rm -rf /var/lib/apt/lists/* \
-	&& localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
-ENV LANG en_US.utf8
-
-# postgres build deps
-RUN apt-get update && apt-get install -y \
-	git \
-	make \
-	gcc \
-	gdb \
-	libreadline-dev \
-	bison \
-	flex \
-	zlib1g-dev \
-	sudo \
-	&& rm -rf /var/lib/apt/lists/*
-
-RUN mkdir /pg && chown postgres:postgres /pg
-# We need that to allow editing of /proc/sys/kernel/core_pattern
-# from docker-entrypoint.sh
-RUN echo "postgres ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-COPY ./ /pg/src
-RUN chown -R postgres:postgres /pg/src
+# there is already accidental postgres user in alpine, so call ours pg =/
+# RUN addgroup pg && adduser -h /pg -D -G pg pg
+RUN mkdir /pg && chown postgres:postgres pg
 
 USER postgres
+
+ENV LANG en_US.utf8
 ENV CFLAGS -O0
-WORKDIR /pg
+ENV PATH /pg/install/bin:$PATH
+
+COPY ./ /pg/src
 
 RUN cd /pg/src && \
-	ls -la && \
-	whoami && \
 	./configure  --enable-cassert --enable-debug --prefix=/pg/install && \
 	make -j 4 install
-
-ENV PATH /pg/install/bin:$PATH
-ENV PGDATA /pg/data
-
-RUN cd /pg/src/contrib/raftable && make clean && make install
-RUN cd /pg/src/contrib/mmts && make clean && make install
-
-ENTRYPOINT ["/pg/src/contrib/mmts/tests2/docker-entrypoint.sh"]
-
-EXPOSE 5432
-CMD ["postgres"]
--- contrib/mmts/tests2/docker-entrypoint.sh
+++ contrib/mmts/tests2/docker-entrypoint.sh
@@ -1,89 +1,62 @@
-#!/bin/bash
-set -e
-
-# wtf is that?
-if [ "${1:0:1}" = '-' ]; then
-	set -- postgres "$@"
-fi
-
-echo "/pg/%p.%s.%c.%P.core" | sudo tee /proc/sys/kernel/core_pattern
+#!/bin/sh
 
 if [ "$1" = 'postgres' ]; then
 	mkdir -p "$PGDATA"
-	chmod 700 "$PGDATA"
-	chown -R postgres "$PGDATA"
 
 	# look specifically for PG_VERSION, as it is expected in the DB dir
 	if [ ! -s "$PGDATA/PG_VERSION" ]; then
 		initdb --nosync
 
-		if [ "$POSTGRES_PASSWORD" ]; then
-			pass="PASSWORD '$POSTGRES_PASSWORD'"
-			authMethod=md5
-		else
-			pass=
-			authMethod=trust
-		fi
+		{ echo; echo "host all all 0.0.0.0/0 trust"; } >> "$PGDATA/pg_hba.conf"
+		{ echo; echo "host replication all 0.0.0.0/0 trust"; } >> "$PGDATA/pg_hba.conf"
 
-		{ echo; echo "host all all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
-		{ echo; echo "host replication all 0.0.0.0/0 $authMethod"; } >> "$PGDATA/pg_hba.conf"
-
-		############################################################################
-
-		# internal start of server in order to allow set-up using psql-client		
+		# internal start of server in order to allow set-up using psql-client
 		# does not listen on TCP/IP and waits until start finishes
 		pg_ctl -D "$PGDATA" \
 			-o "-c listen_addresses=''" \
 			-w start
 
-		: ${POSTGRES_USER:=postgres}
-		: ${POSTGRES_DB:=$POSTGRES_USER}
-		export POSTGRES_USER POSTGRES_DB
-
-		psql=( psql -v ON_ERROR_STOP=1 )
+		# : ${POSTGRES_USER:=postgres}
+		# : ${POSTGRES_DB:=$POSTGRES_USER}
+		# export POSTGRES_USER POSTGRES_DB
 
 		if [ "$POSTGRES_DB" != 'postgres' ]; then
-			"${psql[@]}" --username postgres <<-EOSQL
+			psql -U `whoami` postgres <<-EOSQL
 				CREATE DATABASE "$POSTGRES_DB" ;
 			EOSQL
 			echo
 		fi
 
-		if [ "$POSTGRES_USER" = 'postgres' ]; then
+		if [ "$POSTGRES_USER" = `whoami` ]; then
 			op='ALTER'
 		else
 			op='CREATE'
 		fi
-		"${psql[@]}" --username postgres <<-EOSQL
-			$op USER "$POSTGRES_USER" WITH SUPERUSER $pass ;
+
+		psql -U `whoami` postgres <<-EOSQL
+			$op USER "$POSTGRES_USER" WITH SUPERUSER PASSWORD '';
 		EOSQL
 		echo
 
 		############################################################################
-		
-		CONNSTRS='dbname=postgres user=postgres host=node1, dbname=postgres user=postgres host=node2, dbname=postgres user=postgres host=node3'
-		RAFT_PEERS='1:node1:6666, 2:node2:6666, 3:node3:6666'
+
+		CONNSTRS="dbname=$POSTGRES_DB user=$POSTGRES_USER host=node1, dbname=$POSTGRES_DB user=$POSTGRES_USER host=node2, dbname=$POSTGRES_DB user=$POSTGRES_USER host=node3"
 
 		cat <<-EOF >> $PGDATA/postgresql.conf
 			listen_addresses='*' 
 			max_prepared_transactions = 100
 			synchronous_commit = on
 			fsync = off
-            log_line_prefix = '%t: '
 			wal_level = logical
 			max_worker_processes = 30
 			max_replication_slots = 10
 			max_wal_senders = 10
 			shared_preload_libraries = 'raftable,multimaster'
 			default_transaction_isolation = 'repeatable read'
-			log_checkpoints = on
-			checkpoint_timeout = 30
-			log_autovacuum_min_duration = 0
 
 			multimaster.workers = 4
-            multimaster.use_raftable = false
 			multimaster.max_nodes = 3
-			multimaster.use_raftable = true
+			multimaster.use_raftable = false
 			multimaster.queue_size=52857600
 			multimaster.ignore_tables_without_pk = 1
 			multimaster.node_id = $NODE_ID
@@ -93,15 +66,8 @@ if [ "$1" = 'postgres' ]; then
 			multimaster.twopc_min_timeout = 2000
 		EOF
 
-		tail -n 20 $PGDATA/postgresql.conf
-
 		pg_ctl -D "$PGDATA" -m fast -w stop
-
-		echo
-		echo 'PostgreSQL init process complete; ready for start up.'
-		echo
 	fi
 fi
 
 exec "$@"
-
