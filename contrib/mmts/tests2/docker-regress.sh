dac6e9f27026631da020d5c98d6d4d7e6af744c2
Stas Kelvich <stas.kelvich@gmail.com>
2016-10-24 18:19:06 +0300

    Catch exit status in regression test.

--- contrib/mmts/tests2/docker-regress.sh
+++ contrib/mmts/tests2/docker-regress.sh
@@ -2,12 +2,14 @@
 
 cd /pg/src/src/test/regress
 
-cat /etc/hosts
-
 ./pg_regress --use-existing \
     --schedule=parallel_schedule \
     --host=node1 \
     --user=postgres \
     --dlpath=/pg/src/src/test/regress/
 
+STATUS = $?
+
 cat regression.diffs
+
+exit $STATUS
