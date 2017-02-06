abc12748ad175e08194d352aabd01090a7eac4c5
Vladimir Ershov <v.ershov@postgrespro.ru>
2017-02-06 16:39:52 +0300

    fix tests

--- sql/cron_string.sql
+++ sql/cron_string.sql
@@ -39,7 +39,7 @@ select schedule.create_job(
 	}'
 );
 
-select * from schedule.cron order by id;
+select id, node, name, rule, do_sql, same_transaction, postpone, retry from schedule.cron order by id;
 
 RESET SESSION AUTHORIZATION;
 drop user __temp_root;
