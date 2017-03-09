eaa44ea9af7696e685a7e27a4a8abd5460bf4501
Stas Kelvich <stas.kelvich@gmail.com>
2017-03-09 20:33:39 +0300

    alter_sequences() function

--- contrib/mmts/multimaster--1.0.sql
+++ contrib/mmts/multimaster--1.0.sql
@@ -84,3 +84,40 @@ LANGUAGE C;
 
 CREATE TABLE IF NOT EXISTS mtm.local_tables(rel_schema text, rel_name text, primary key(rel_schema, rel_name));
 
+CREATE OR REPLACE FUNCTION mtm.alter_sequences() RETURNS boolean AS 
+$$
+DECLARE
+    seq_class pg_class%rowtype;
+    seq_tuple record;
+    node_id int;
+    max_nodes int;
+    new_start int;
+    altered boolean := false;
+BEGIN
+    select current_setting('multimaster.max_nodes') into max_nodes;
+    select id, "allNodes" into node_id from mtm.get_cluster_state();
+    FOR seq_class IN
+        SELECT * FROM pg_class WHERE pg_class.relkind = 'S'
+    LOOP
+        EXECUTE 'select * from ' || seq_class.relname || ';' INTO seq_tuple;
+        IF seq_tuple.increment_by != max_nodes THEN
+            altered := true;
+            RAISE NOTICE 'Altering step for sequence % to %.', seq_tuple.sequence_name, max_nodes;
+            EXECUTE 'ALTER SEQUENCE ' || seq_class.relname || ' INCREMENT BY ' || max_nodes || ';';
+        END IF;
+        IF (seq_tuple.last_value % max_nodes) != node_id THEN
+            altered := true;
+            new_start := (seq_tuple.last_value / max_nodes + 1)*max_nodes + node_id;
+            RAISE NOTICE 'Altering start for sequence % to %.', seq_tuple.sequence_name, new_start;
+            EXECUTE 'ALTER SEQUENCE ' || seq_class.relname || ' RESTART WITH ' || new_start || ';';
+        END IF;
+    END LOOP;
+    IF altered = false THEN
+        RAISE NOTICE 'All found sequnces have proper params.';
+    END IF;
+    RETURN true;
+END
+$$
+LANGUAGE plpgsql;
+
+select mtm.alter_sequences();
