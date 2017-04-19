bea7ac9056ea8953dd7d827ebab53046e07f5e88
Konstantin Knizhnik <knizhnik@garret.ru>
2017-04-19 16:53:06 +0300

    Add reinit-pathman.sh

---
+++ contrib/postgres_fdw/tests/reinit-pathman.sh
@@ -0,0 +1,48 @@
+n_shards=3
+export PATH=~/postgres_cluster/dist/bin/:$PATH
+ulimit -c unlimited
+pkill -9 postgres
+sleep 2
+rm -fr master shard? *.log pg_worker_list.conf
+for ((i=1;i<=n_shards;i++))
+do    
+    port=$((5432+i))
+    echo "localhost $port" >> pg_worker_list.conf
+    initdb shard$i
+done
+initdb master
+cp pg_worker_list.conf master
+
+echo Start shards
+
+for ((i=1;i<=n_shards;i++))
+do
+    port=$((5432+i))
+    sed "s/5432/$port/g" < postgresql.conf.fdw > shard$i/postgresql.conf
+	echo "shared_preload_libraries = 'pg_tsdtm'" >> shard$i/postgresql.conf
+    pg_ctl -D shard$i -l shard$i.log start
+done
+
+echo Start master
+cp postgresql.conf.fdw master/postgresql.conf
+echo "shared_preload_libraries = 'pg_pathman'" >> master/postgresql.conf
+pg_ctl -D master -l master.log start
+
+sleep 5
+
+user=`whoami` 
+psql postgres -c "CREATE EXTENSION postgres_fdw"
+psql postgres -c "CREATE EXTENSION pg_pathman"
+psql postgres -c "CREATE TABLE t(u integer primary key, v integer)"
+
+for ((i=1;i<=n_shards;i++))
+do
+    port=$((5432+i))
+	psql -p $port postgres -c "CREATE EXTENSION pg_tsdtm"
+	psql -p $port postgres -c "CREATE TABLE t(u integer primary key, v integer)"
+
+	psql postgres -c "CREATE SERVER shard$i TYPE 'shardman' FOREIGN DATA WRAPPER postgres_fdw options(dbname 'postgres', host 'localhost', port '$port')"
+	psql postgres -c "CREATE USER MAPPING for $user SERVER shard$i options (user '$user')"
+done
+
+psql postgres -c "select create_shards('t', 'u', 10, 'shardman')"
