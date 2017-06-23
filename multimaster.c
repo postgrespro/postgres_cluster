74f28a2251c1f6296e83ce1a50be50a3c31c7d41
Konstantin Knizhnik <knizhnik@garret.ru>
2017-06-23 15:29:16 +0300

    Perform drop node under lock

--- multimaster.c
+++ multimaster.c
@@ -371,6 +371,11 @@ void MtmLockNode(int nodeId, LWLockMode mode)
 	LWLockAcquire((LWLockId)&Mtm->locks[nodeId], mode);
 }
 
+bool MtmTryLockNode(int nodeId, LWLockMode mode)
+{
+	return LWLockConditionalAcquire((LWLockId)&Mtm->locks[nodeId], mode);
+}
+
 void MtmUnlockNode(int nodeId)
 {
 	Assert(nodeId > 0 && nodeId <= MtmMaxNodes*2);
@@ -1684,7 +1689,14 @@ static void MtmStartRecovery()
 
 static void MtmDropSlot(int nodeId)
 {
-	ReplicationSlotDrop(psprintf(MULTIMASTER_SLOT_PATTERN, nodeId));
+	if (MtmTryLockNode(nodeId, LW_EXCLUSIVE))
+	{
+		MTM_ELOG(INFO, "Drop replication slot for node %d", nodeId);
+		ReplicationSlotDrop(psprintf(MULTIMASTER_SLOT_PATTERN, nodeId));
+		MtmUnlockNode(nodeId);
+	} else {
+		MTM_ELOG(WARNING, "Failed to drop replication slot for node %d", nodeId);
+	}
 	MtmLock(LW_EXCLUSIVE);
 	BIT_SET(Mtm->stalledNodeMask, nodeId-1);
 	BIT_SET(Mtm->stoppedNodeMask, nodeId-1); /* stalled node can not be automatically recovered */
@@ -2766,7 +2778,7 @@ void MtmUpdateNodeConnectionInfo(MtmConnectionInfo* conn, char const* connStr)
 	} else {
 		conn->arbiterPort = MULTIMASTER_DEFAULT_ARBITER_PORT;
 	}
-	MTM_ELOG(WARNING, "Using arbiter port: %d", conn->arbiterPort);
+	MTM_ELOG(INFO, "Using arbiter port: %d", conn->arbiterPort);
 
 	port = strstr(connStr, " port=");
 	if (port == NULL && strncmp(connStr, "port=", 5) == 0) {
@@ -3422,7 +3434,7 @@ _PG_init(void)
 	PreviousProcessUtilityHook = ProcessUtility_hook;
 	ProcessUtility_hook = MtmProcessUtility;
 
-	PreviousSeqNextvalHook = SeqNextvalHook; 
+	PreviousSeqNextvalHook = SeqNextvalHook;
 	SeqNextvalHook = MtmSeqNextvalHook;
 }
 
@@ -5380,7 +5392,7 @@ static void MtmSeqNextvalHook(Oid seqid, int64 next)
 		MtmSeqPosition pos;
 		pos.seqid = seqid;
 		pos.next = next;
-		LogLogicalMessage("N", (char*)&pos, sizeof(pos), true);		
+		LogLogicalMessage("N", (char*)&pos, sizeof(pos), true);
 	}
 }
 
@@ -5567,4 +5579,3 @@ Datum mtm_referee_poll(PG_FUNCTION_ARGS)
 
 	PG_RETURN_INT64(recoveredNodeMask);
 }
-
