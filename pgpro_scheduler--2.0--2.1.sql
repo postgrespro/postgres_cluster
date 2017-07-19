1b8cff0fac959761938bbff664bf2efb5b1df092
Vladimir Ershov <v.ershov@postgrespro.ru>
2017-07-19 19:12:06 +0300

    more accurate with schema && get_log from view

--- pgpro_scheduler--2.0--2.1.sql
+++ pgpro_scheduler--2.0--2.1.sql
@@ -1,5 +1,3 @@
-SET search_path TO schedule;
-
 DROP VIEW job_status;
 DROP VIEW all_job_status;
 
@@ -15,7 +13,7 @@ CREATE VIEW job_status AS
 		max_run_time as max_duration, submit_time, canceled,
 		start_time, status as is_success, reason as error, done_time,
 		'done'::job_at_status_t status
-	FROM schedule.at_jobs_done where owner = session_user
+	FROM @extschema@.at_jobs_done where owner = session_user
 		UNION 
 	SELECT
 		id, node, name, comments, at as run_after,
@@ -24,7 +22,7 @@ CREATE VIEW job_status AS
 		max_run_time as max_duration, submit_time, canceled, start_time, 
 		NULL as is_success, NULL as error, NULL as done_time,
 		'processing'::job_at_status_t status
-	FROM ONLY schedule.at_jobs_process where owner = session_user
+	FROM ONLY @extschema@.at_jobs_process where owner = session_user
 		UNION
 	SELECT
 		id, node, name, comments, at as run_after,
@@ -34,7 +32,7 @@ CREATE VIEW job_status AS
 		NULL as start_time, NULL as is_success, NULL as error,
 		NULL as done_time,
 		'submitted'::job_at_status_t status
-	FROM ONLY schedule.at_jobs_submitted where owner = session_user;
+	FROM ONLY @extschema@.at_jobs_submitted where owner = session_user;
 
 CREATE VIEW all_job_status AS 
 	SELECT 
@@ -44,7 +42,7 @@ CREATE VIEW all_job_status AS
 		max_run_time as max_duration, submit_time, canceled,
 		start_time, status as is_success, reason as error, done_time,
 		'done'::job_at_status_t status
-	FROM schedule.at_jobs_done 
+	FROM @extschema@.at_jobs_done 
 		UNION 
 	SELECT
 		id, node, name, comments, at as run_after,
@@ -53,7 +51,7 @@ CREATE VIEW all_job_status AS
 		max_run_time as max_duration, submit_time, canceled, start_time,
 		NULL as is_success, NULL as error, NULL as done_time,
 		'processing'::job_at_status_t status
-	FROM ONLY schedule.at_jobs_process 
+	FROM ONLY @extschema@.at_jobs_process 
 		UNION
 	SELECT
 		id, node, name, comments, at as run_after,
@@ -63,8 +61,165 @@ CREATE VIEW all_job_status AS
 		NULL as start_time, NULL as is_success, NULL as error,
 		NULL as done_time,
 		'submitted'::job_at_status_t status
-	FROM ONLY schedule.at_jobs_submitted;
+	FROM ONLY @extschema@.at_jobs_submitted;
 
-GRANT SELECT ON schedule.job_status TO public;
+GRANT SELECT ON @extschema@.job_status TO public;
 
-RESET search_path;
+DROP FUNCTION get_log();
+DROP FUNCTION get_log(text);
+DROP FUNCTION get_user_log();
+
+
+CREATE INDEX ON cron (owner);
+CREATE INDEX ON cron (executor);
+
+--
+-- show all scheduled jobs 
+--
+CREATE VIEW all_jobs_log AS 
+	SELECT 
+		coalesce(c.id, l.cron) as cron,
+		c.node as node,
+		l.start_at as scheduled_at,
+		coalesce(c.name, '--DELETED--') as name,
+		c.comments as comments,
+		c.do_sql as commands,
+		c.executor as run_as,
+		c.owner as owner,
+		c.same_transaction as use_same_transaction,
+		l.started as started,
+		l.last_start_available as last_start_available,
+		l.finished as finished,
+		c.max_run_time as max_run_time,
+		c.onrollback_statement as onrollback,
+		c.next_time_statement as next_time_statement,
+		c.max_instances as max_instances,
+		CASE WHEN l.status THEN
+			'done'::@extschema@.job_status_t
+		ELSE
+			'error'::@extschema@.job_status_t
+		END as status,
+		l.message as message
+
+	FROM @extschema@.log as l LEFT OUTER JOIN @extschema@.cron as c ON c.id = l.cron;
+
+--
+-- show scheduled jobs of session user
+--
+
+CREATE VIEW jobs_log AS 
+	SELECT
+		coalesce(c.id, l.cron) as cron,
+		c.node as node,
+		l.start_at as scheduled_at,
+		coalesce(c.name, '--DELETED--') as name,
+		c.comments as comments,
+		c.do_sql as commands,
+		c.executor as run_as,
+		c.owner as owner,
+		c.same_transaction as use_same_transaction,
+		l.started as started,
+		l.last_start_available as last_start_available,
+		l.finished as finished,
+		c.max_run_time as max_run_time,
+		c.onrollback_statement as onrollback,
+		c.next_time_statement as next_time_statement,
+		c.max_instances as max_instances,
+		CASE WHEN l.status THEN
+			'done'::@extschema@.job_status_t
+		ELSE
+			'error'::@extschema@.job_status_t
+		END as status,
+		l.message as message
+	FROM log as l, cron as c WHERE c.executor = session_user AND c.id = l.cron;
+
+
+CREATE FUNCTION get_log(usename text) RETURNS 
+	table(
+		cron int,
+		node text,
+		scheduled_at timestamp with time zone,
+		name text,
+		comments text,
+		commands text[],
+		run_as text,
+		owner text,
+		use_same_transaction boolean,
+		started timestamp with time zone,
+		last_start_available timestamp with time zone,
+		finished timestamp with time zone,
+		max_run_time interval,
+		onrollback text,
+		next_time_statement text,
+		max_instances integer,
+		status @extschema@.job_status_t,
+		message text
+	)
+AS
+$BODY$
+ 	SELECT * FROM @extschema@.all_jobs_log where owner = usename;
+$BODY$
+LANGUAGE sql STABLE; 
+
+
+
+CREATE FUNCTION get_log() RETURNS 
+	table(
+		cron int,
+		node text,
+		scheduled_at timestamp with time zone,
+		name text,
+		comments text,
+		commands text[],
+		run_as text,
+		owner text,
+		use_same_transaction boolean,
+		started timestamp with time zone,
+		last_start_available timestamp with time zone,
+		finished timestamp with time zone,
+		max_run_time interval,
+		onrollback text,
+		next_time_statement text,
+		max_instances integer,
+		status @extschema@.job_status_t,
+		message text
+	)
+AS
+$BODY$
+ 	SELECT * FROM @extschema@.all_jobs_log;
+$BODY$
+LANGUAGE sql STABLE; 
+
+
+CREATE FUNCTION get_user_log() RETURNS 
+	table(
+		cron int,
+		node text,
+		scheduled_at timestamp with time zone,
+		name text,
+		comments text,
+		commands text[],
+		run_as text,
+		owner text,
+		use_same_transaction boolean,
+		started timestamp with time zone,
+		last_start_available timestamp with time zone,
+		finished timestamp with time zone,
+		max_run_time interval,
+		onrollback text,
+		next_time_statement text,
+		max_instances integer,
+		status @extschema@.job_status_t,
+		message text
+	)
+AS
+$BODY$
+ 	SELECT * FROM @extschema@.jobs_log;
+$BODY$
+LANGUAGE sql STABLE; 
+
+GRANT SELECT ON @extschema@.jobs_log TO public;
+
+ALTER TABLE @extschema@.at ADD PRIMARY KEY (start_at, cron);
+ALTER TABLE @extschema@.log ADD PRIMARY KEY (start_at, cron);
+
