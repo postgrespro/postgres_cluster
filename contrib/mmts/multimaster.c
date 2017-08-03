acaad1cee88778f16207c5a6eba666d96b8500d0
Stas Kelvich <stas.kelvich@gmail.com>
2017-08-03 12:14:35 +0300

    Add ALTER SYSTEM to list of not replicated DDL

--- contrib/mmts/multimaster.c
+++ contrib/mmts/multimaster.c
@@ -5114,6 +5114,7 @@ static void MtmProcessUtility(Node *parsetree, const char *queryString,
 		case T_CheckPointStmt:
 		case T_ReindexStmt:
 		case T_ExplainStmt:
+		case T_AlterSystemStmt:
 			skipCommand = true;
 			break;
 
