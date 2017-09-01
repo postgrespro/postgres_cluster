faf90ea1d874fd613aa6aabee8b6ef416a9309ed
Stas Kelvich <stas.kelvich@gmail.com>
2017-09-01 11:32:01 +0300

    Fix 'aborted on node #' error message

--- arbiter.c
+++ arbiter.c
@@ -1078,7 +1078,7 @@ static void MtmReceiver(Datum arg)
 							if (ts->status != TRANSACTION_STATUS_ABORTED) { 
 								MTM_LOG1("Arbiter receive abort message for transaction %s (%llu) from node %d", ts->gid, (long64)ts->xid, node);
 								Assert(ts->status == TRANSACTION_STATUS_IN_PROGRESS);
-								ts->aborted_by_node = node;
+								ts->abortedByNode = node;
 								MtmAbortTransaction(ts);
 							}
 							if ((ts->participantsMask & ~Mtm->disabledNodeMask & ~ts->votedMask) == 0) {
