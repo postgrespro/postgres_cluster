4fc008b8b0f1b0af722f6ff3a80dfd5c203f67a0
Stas Kelvich <stas.kelvich@gmail.com>
2017-09-21 20:27:45 +0300

    assign csn for local transactions. Fix by kk

--- contrib/mmts/multimaster.c
+++ contrib/mmts/multimaster.c
@@ -1529,6 +1529,10 @@ MtmEndTransaction(MtmCurrentTrans* x, bool commit)
 					ts->csn = x->csn;
 					MtmSyncClock(ts->csn);
 				}
+				if (ts->isLocal)
+				{
+					ts->csn = MtmAssignCSN();
+				}
 				Mtm->lastCsn = ts->csn;
 				ts->status = TRANSACTION_STATUS_COMMITTED;
 				MtmAdjustSubtransactions(ts);
