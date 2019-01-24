e75ed95edead8c8f42723fb8789a0ed6a5ac975f
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2019-01-24 15:04:17 +0500

    Add pg_execplan extension

--- contrib/pg_execplan/init.sql
+++ contrib/pg_execplan/init.sql
@@ -1,1 +1,5 @@
-\echo Use "CREATE EXTENSION repeater" to load this file. \quit
+\echo Use "CREATE EXTENSION pg_execplan" to load this file. \quit
+
+CREATE OR REPLACE FUNCTION @extschema@.pg_store_query_plan(query TEXT)
+RETURNS VOID AS 'pg_execplan'
+LANGUAGE C;
