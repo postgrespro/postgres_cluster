c3ec548434ace70405d7549e0cf90ccdb3d4a426
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2019-01-25 16:47:34 +0500

    Add store and exec plan functions

--- init.sql
+++ init.sql
@@ -1,5 +1,14 @@
 \echo Use "CREATE EXTENSION pg_execplan" to load this file. \quit
 
-CREATE OR REPLACE FUNCTION @extschema@.pg_store_query_plan(query TEXT)
+-- Store plan of a query into a text file.
+-- query - query string which will be parsed and planned.
+-- filename - path to the file on a disk.
+CREATE OR REPLACE FUNCTION @extschema@.pg_store_query_plan(
+														query TEXT,
+														filename TEXT)
+RETURNS VOID AS 'pg_execplan'
+LANGUAGE C;
+
+CREATE OR REPLACE FUNCTION @extschema@.pg_exec_query_plan(filename TEXT)
 RETURNS VOID AS 'pg_execplan'
 LANGUAGE C;
