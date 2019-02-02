910d7764dbab2b7e7e0476894ff5226a1151af0e
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2019-02-02 12:15:23 +0500

    Add 6 more OID typeds at portable plan

--- tests/rpl.sh
+++ tests/rpl.sh
@@ -78,3 +78,17 @@ psql -p 5433 -c "SELECT oid, oprname, oprnamespace FROM pg_operator WHERE oprnam
 # Test
 psql -p 5432 -c "SELECT pg_store_query_plan('../test.txt', 'SELECT id ### 1 FROM tests.ttest1;');"
 psql -p 5433 -c "SELECT pg_exec_stored_plan('../test.txt');"
+
+#ENUMOID -----------------------------------------------------------------------
+psql -p 5432 -c "CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy');"
+psql -p 5432 -c "CREATE TABLE person ( name text, current_mood mood);"
+psql -p 5433 -c "CREATE TYPE mood AS ENUM ('sad', 'ok', 'happy');"
+psql -p 5433 -c "CREATE TABLE person ( name text, current_mood mood);"
+
+psql -p 5432 -c "SELECT pg_store_query_plan('../test.txt', 'INSERT INTO person VALUES (''Moe'', ''happy'');');"
+psql -p 5432 -c "SELECT pg_exec_stored_plan('../test.txt');"
+psql -p 5433 -c "SELECT pg_exec_stored_plan('../test.txt');"
+
+psql -p 5432 -c "SELECT * FROM person WHERE current_mood = 'happy';"
+psql -p 5433 -c "SELECT * FROM person WHERE current_mood = 'happy';"
+
