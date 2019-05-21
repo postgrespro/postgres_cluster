edf22c53361ac165ee1e7b1c974534c970e977bd
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2019-05-21 09:18:54 +0500

    -

--- contrib/pg_exchange/gcp/paths.sh
+++ contrib/pg_exchange/gcp/paths.sh
@@ -1,5 +1,7 @@
 INSTDIR=`pwd`/tmp_install
+GCPSCRIPTS=`pwd`/contrib/pg_exchange.gcp/
 
 export LD_LIBRARY_PATH=$INSTDIR/lib:$LD_LIBRARY_PATH
 export PATH=$INSTDIR/bin:$PATH
+export PATH=$GCPSCRIPTS:$PATH
 
