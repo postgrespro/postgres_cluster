5476e856126772c9c53e7c929a7f1aac2e98a67b
Andrey V. Lepikhov <a.lepikhov@postgrespro.ru>
2019-05-21 09:56:10 +0500

    -

--- contrib/pg_exchange/gcp/init.sh
+++ contrib/pg_exchange/gcp/init.sh
@@ -15,23 +15,23 @@ export PGDATABASE=test
 pkill -9 -e postgres || true
 sleep 1
 
-rm -rf $GDATA
-mkdir $GDATA
+rm -rf $PGDATA
+mkdir $PGDATA
 rm -rf logfile.log || true
 
 # Building project
-./cmp.sh
+
 make > /dev/null
 make -C contrib > /dev/null
 make install > /dev/null
 make -C contrib install > /dev/null
 
-initdb -D GCP_PGDATA -E UTF8 --locale=C
+initdb -D $PGDATA -E UTF8 --locale=C
 
 echo "shared_preload_libraries = 'postgres_fdw, pg_exchange'" >> $PGDATA/postgresql.conf
 echo "listen_addresses = '*'" >> $PGDATA/postgresql.conf
 echo "host    all             all             0.0.0.0/0                 trust" >> $PGDATA/pg_hba.conf
 
-pg_ctl -w -c -D $PGDATA -l logfile-$1.log start
+pg_ctl -w -c -D $PGDATA -l logfile.log start
 createdb
 
